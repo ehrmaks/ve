274382204fd237d96685893db5678238dcfb3e6a
starlabs-jskim91 <jskim91@starlabs.co.kr>
2020-12-20 18:32:57 +0900

    add editor

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,22 @@
 module.exports = {
-  transpileDependencies: ["vuetify"]
+  transpileDependencies: ["vuetify"],
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        appId: "com.jookbob2.redstone",
+        nsis: {
+          shortcutName: "Red stone"
+        },
+        win: {
+          target: [
+            {
+              target: "nsis",
+              arch: ["x64", "ia32"]
+            }
+          ]
+        }
+      }
+    }
+  }
 };
